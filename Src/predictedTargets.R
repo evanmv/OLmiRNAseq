d033d5d420440c1f36096547e9f6a2079e3ae9a7
evanmv <evanmv@uio.no>
2024-10-17 11:26:13 +0200

    Updated function and merge target results

--- Src/predictedTargets.R
+++ Src/predictedTargets.R
@@ -1,5 +1,7 @@
 library(readxl)
 library(tidyverse)
+install.packages("plyr")
+library(plyr)
 
 miR129_miRDB <- read_xlsx("Data/miR-129-2-3p-miRDB.xlsx")
 miR129_TS <- read.delim("Data/TargetScan8.0__miR-129-3p.Human.predicted_targets.txt")
@@ -9,19 +11,30 @@ miR129_Targets <- merge.f$Target.gene
 
 #for loop? or function?
 write_lines(miR129_Targets, "Res/miR129_Targets.txt") #May be better to combine all DEmiRs into table and write out
-targetList <- function(miRDB, TargetScan) {
-  miRDB <- read_xlsx(miRDB)
+targetList <- function(Target) {
+  miRDB <- read_xlsx(paste0("Data/miRDB/",Target,".xlsx"))
   
-  miR_TS <- read.delim(TargetScan)
+  miR_TS <- read.delim(paste0("Data/TargetScan/",Target,".txt"))
   
   merge <- miR_TS %>%
     filter(miR_TS$Target.gene %in% miRDB$'Gene Symbol')
   merge <- merge %>%
     filter(Cumulative.weighted.context...score < -0.5 & Aggregate.PCT > 0.5)
   
-  lbl_Targets <- merge$Target.gene
+  Target <- merge$Target.gene %>%
+    as_tibble() %>%
+    `colnames<-`(Target)
     
 }
 
-miR1292 <- targetList("Data/miR-129-2-3p-miRDB.xlsx", "Data/TargetScan8.0__miR-129-3p.Human.predicted_targets.txt")
+miR129 <- targetList('129-2-3p')
+miR204 <- targetList('204-5p')
+miR210 <- targetList('210-5p')
+miR320 <- targetList('320')
+miR375 <- targetList('375')
+miR3065 <- targetList('3065-5p')
+
+x <- merge.data.frame(miR129, miR204, by = 0, all = T)[-1]
+
+df <- join_all(list(miR129, miR204, miR210,miR320, miR375,miR3065), by = 0, type = 'full')
 

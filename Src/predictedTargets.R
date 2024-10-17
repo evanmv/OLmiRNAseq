8c52698deba8aea08135b920b952369cf71da53d
evanmv <evanmv@uio.no>
2024-10-17 11:31:34 +0200

    Cleanup

--- Src/predictedTargets.R
+++ Src/predictedTargets.R
@@ -3,14 +3,7 @@ library(tidyverse)
 install.packages("plyr")
 library(plyr)
 
-miR129_miRDB <- read_xlsx("Data/miR-129-2-3p-miRDB.xlsx")
-miR129_TS <- read.delim("Data/TargetScan8.0__miR-129-3p.Human.predicted_targets.txt")
-merge <- filter(miR129_TS, miR129_TS$Target.gene %in% miR129_miRDB$`Gene Symbol`)
-merge.f <- filter(merge, merge$Cumulative.weighted.context...score < -0.5 & merge$Aggregate.PCT > 0.5)
-miR129_Targets <- merge.f$Target.gene
-
-#for loop? or function?
-write_lines(miR129_Targets, "Res/miR129_Targets.txt") #May be better to combine all DEmiRs into table and write out
+#Function to read in data from miRDB and TargetScan, compare, and filter for score and conservation. Outputs tibble with gene names
 targetList <- function(Target) {
   miRDB <- read_xlsx(paste0("Data/miRDB/",Target,".xlsx"))
   
